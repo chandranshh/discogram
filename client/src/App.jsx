af19b5d0fd85e1f51e779b8d9bda6e879ced939f
chandranshh <chandranshyay@gmail.com>
2023-05-20 01:16:35 +0530

    almost done with the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import Form from "./modules/Form/Form";
 import Dashboard from "./modules/Dashboard/Dashboard";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("user:token") !== null || true;
+
+  if (!isLoggedIn) {
+    return <Navigate to={"/"} />;
+  } else if (isLoggedIn && ["/"].includes(window.location.pathname)) {
+    return <Navigate to={"/dashboard"} />;
+  }
+  return children;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/users/register" element={<Form />} />
-      <Route spath="/users/login" element={<Form />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Form />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
